10906f9ba9c987c8b00ed4959901d0c6233e9005
Nation Chirara <chiraranw@gmail.com>
2018-07-23 18:55:22 +0100

    A Bootstrap Project. An incomplete Blog

---
+++ kimblog/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link href="css/bootstrap-3.3.7.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/custom.css">
+
+
+<title>BLOG</title>
+
+<body style="padding-top: 50px" id="home">
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"
+       aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar">
+       </span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" id="brand" href="#home"><span class="title">KIMBLOG</span></a></div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse no-brackets" id="topFixedNavbar1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="nav-no-padding-on-list"> <a href="#home">Home</a></li>
+        <li ><a href="#news">Fashion</a></li>
+        <li ><a href="#tm">Education</a></li>
+        <li ><a href="#gallery">Gallery</a></li>
+        <li><a href="#about">Abaout Me</a></li>
+        <li><a href="#contact">Contact</a></li> 
+        <li><a data-toggle="modal" href="#login-modal">Login</a></li> 
+      </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+  </div>
+  <!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+	<div class="jumbotron">
+
+	</div>
+</div>
+
+
+
+
+
+
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap-3.3.7.js"></script>
+
+
+
+</body>
+</html>
