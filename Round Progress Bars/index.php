cc0b2127da72075c9ea2ab1c132f98c4d3b745f9
Nation Chirara <chiraranw@gmail.com>
2018-07-23 19:01:10 +0100

    Bootstrap Projetc: Demonstrating Round Progress Bars with %

---
+++ Round Progress Bars/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link href="css/bootstrap-3.3.7.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/progress-circle.min.css">
+<title>Panel Customized</title>
+<body>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="progress-circle progress-79"><span>75</span></div>
+      </div>
+      <div class="col-md-3">
+        <div class="progress-circle progress-50">
+          <span class="pc">75</span>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="progress-circle progress-99"><span>75</span></div>
+      </div>
+    </div>
+  </div>
+
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/bootstrap-3.3.7.js"></script>
+</body>
+</html>
