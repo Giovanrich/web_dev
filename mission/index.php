9e32e040279ec1f837cc06beb566c1e0d91af397
Nation Chirara <chiraranw@gmail.com>
2018-08-05 20:15:23 +0100

    BS3: Just a nice way of presenting 'Mission' section for a site.

---
+++ mission/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--The above three must come first!-->
+
+	<title>High School</title>
+	<!--Bootstrap Core CSS-->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<!--Custome CSS-->
+  <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+
+    
+
+</head>
+
+<body>
+
+
+
+      <div class="col-xs-12 col-sm-8 wow fadeInDown  animated" data-wow-duration="1000ms" data-wow-delay="1800ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 1800ms; animation-name: fadeInDown;">
+                   <div class="tab-wrap"> 
+                        <div class="media">
+                            <div class="parrent pull-left">
+                                <ul class="nav nav-tabs nav-stacked">
+                                    <li class=""><a href="#tab1" data-toggle="tab" class="analistic-01">Core Business &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+                                    <li class=""><a href="#tab2" data-toggle="tab" class="analistic-02">Our Mission</a></li>
+                                    <li class=""><a href="#tab3" data-toggle="tab" class="tehnical">Our Vission</a></li>
+                                    <li class="active"><a href="#tab4" data-toggle="tab" class="tehnical">Values</a></li>
+                                    
+                                </ul>
+                            </div>
+
+                            <div class="parrent media-body">
+                                <div class="tab-content">
+                                    <div class="tab-pane fade" id="tab1">
+                                        <div class="media">
+                                          
+                                            <div class="media-body">
+                                                
+                                                 <p>Our core business is the analysis and development of business processes with the result of efficient processes; faster, more useful and more comprehensive information to stakeholders. We do this from an IT/computing services angle. </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                     <div class="tab-pane fade" id="tab2">
+                                        <div class="media">
+                                          
+                                            <div class="media-body">
+                                                 
+                                                 <p>To provide systems which avail answers and convenience through innovative data manipulation and information communication systems.
+                                                 </p>
+                                            </div>
+                                        </div>
+                                     </div>
+
+                                     <div class="tab-pane fade" id="tab3">
+                                        <p>To be a world class provider of IT consultancy services.</p>
+<p></p>
+                                     </div>
+                                     
+                                     <div class="tab-pane fade active in" id="tab4">
+                                        <strong>Passion</strong> - we will do it enthusiastically.<br>
+    <strong>Help</strong> - what we do must actually help you.<br>
+    <strong>Integrity</strong> - we are sincere about it all.<br>
+<br>
+  <p>*We develop systems to uphold these values. And we build them rapidly. </p>
+                                     </div>
+
+                                </div> <!--/.tab-content-->  
+                            </div> <!--/.media-body--> 
+                        </div> <!--/.media-->     
+                    </div><!--/.tab-wrap-->               
+                </div>
+
+     <!--Bootstrap Core JS-->
+
+	<script src="assets/js/jquery-3.1.1.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+  
+</body>
+</html>
+
